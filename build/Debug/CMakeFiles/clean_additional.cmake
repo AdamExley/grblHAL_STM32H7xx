3369af5e3e0aa8b6e58108a7161e2a9c7e5de608
Adam Exley <exley2020@gmail.com>
2025-12-27 16:23:46 -0700

    First steps towards CMake-only

---
+++ build/Debug/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,8 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+  file(REMOVE_RECURSE
+  "SKR3_GRBL_HAL.map"
+  )
+endif()
